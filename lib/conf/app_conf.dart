66fc2cb946aaa55afdc2634055d70de61b1baa21
jawa0919 <jawa0919@163.com>
2025-10-09 10:19:56 +0800

    feat(微信登录): 添加微信登录功能及相关配置
    - 在app_conf.dart中添加微信AppId配置
    - 添加fluwx依赖并配置AndroidManifest权限
    - 实现微信登录逻辑及UI交互
    - 配置fluwx相关参数包括iOS Universal Link

--- lib/conf/app_conf.dart
+++ lib/conf/app_conf.dart
@@ -4,6 +4,7 @@ class AppConf {
   static const String applicationId = 'top.meu6jia.xm_util';
   static const String bundleId = 'top.meu6jia.xm_util';
   static const String appStoreId = '';
+  static const String wxAppId = 'wxd96f9ed754396015';
 
   static const bool isProduct = bool.fromEnvironment('dart.vm.product');
 }
--- lib/page/main/main_page.dart
+++ lib/page/main/main_page.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 
 import 'package:app_links/app_links.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluwx/fluwx.dart';
+
+import '../../conf/app_conf.dart';
 
 class MainPage extends StatefulWidget {
   static const String routeName = '/main';
@@ -14,32 +16,70 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   final appLinks = AppLinks();
-  StreamSubscription<Uri>? sub;
+  StreamSubscription<Uri>? linkSub;
+  final fluwx = Fluwx();
+  FluwxCancelable? fluwxSub;
 
   @override
   void initState() {
     super.initState();
     debugPrint('main_page.dart~initState: ');
-    sub = appLinks.uriLinkStream.listen((uri) {
+    linkSub = appLinks.uriLinkStream.listen((uri) {
       debugPrint('main_page.dart~initState: $uri');
     });
   }
 
   @override
   void dispose() {
-    sub?.cancel();
+    linkSub?.cancel();
+    fluwxSub?.cancel();
     super.dispose();
+  }
+
+  void wxLogin(BuildContext context) async {
+    await fluwx.registerApi(appId: AppConf.wxAppId);
+    var isInstalled = await fluwx.isWeChatInstalled.catchError((e) {
+      debugPrint('main_page.dart~wxLogin: $e');
+      return false;
+    });
+    if (!isInstalled) {
+      debugPrint('main_page.dart: 微信未安装');
+      // ignore: use_build_context_synchronously
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('微信未安装'), duration: Duration(seconds: 2)),
+      );
+      return;
+    }
+    fluwxSub = fluwx.addSubscriber((r) {
+      if (r is WeChatAuthResponse) {
+        debugPrint('main_page.dart~addSubscriber: ${r.state} ${r.code}');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('微信${r.state} ${r.code}'),
+            duration: const Duration(seconds: 2),
+          ),
+        );
+      }
+    });
+    var w = NormalAuth(scope: 'snsapi_userinfo', state: 'wechat_sdk_demo_test');
+    await fluwx.authBy(which: w);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('小明工具箱'),
-        centerTitle: true,
-        actions: [Icon(CupertinoIcons.info_circle_fill)],
+      appBar: AppBar(title: Text('小明工具箱')),
+      body: SafeArea(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () => wxLogin(context),
+              child: Text('微信登录'),
+            ),
+            const Placeholder(),
+          ],
+        ),
       ),
-      body: SafeArea(child: const Placeholder()),
     );
   }
 }
